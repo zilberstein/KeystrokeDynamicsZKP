58ce72c78a6a2d83d30e29782f6986c3849c4982
Noam Zilberstein <noamz@seas.upenn.edu>
2014-11-14 14:43:53 -0500

    keystroke measures

--- code/source.js
+++ code/source.js
@@ -1,19 +1,27 @@
 lastTime = 0;
+trials = [{'word':'', 'strokes':[]}];
+tnum = 0;
 
 function recordTime() {
     d = new Date();
     t = d.getTime();
-    if (lastTime === 0) lastTime = t;
+    if (lastTime === 0) {
+        lastTime = t;
+    }
+    trials[tnum].strokes.push(t - lastTime);
     $('#data tr td').last().append(t - lastTime + '<br />');
     lastTime = t;
 }
 
 $('#box').keydown(recordTime);
-$('#box').keyup(recordTime);
+//$('#box').keyup(recordTime);
 
 $('#clear').click(function() {
+    trials[tnum].word = $('#box').val();
     $('#data tr').append('<td></td>');
     $('#box').val('');
     lastTime = 0;
     $('#box').focus();
+    trials.push({'word':'', 'strokes':[]});
+    tnum++;
 });
