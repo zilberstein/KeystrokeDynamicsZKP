9af4b76eb3db728ccbf7f4bb0909f4766d173e74
jpchen <jonathanp.chen@gmail.com>
2014-11-23 15:55:13 -0500

    routes for logging in

--- code/server.py
+++ code/server.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import Flask, render_template, request, g, redirect, url_for, \
-             abort, render_template, flash
+             abort, flash, session
 from contextlib import closing
 
 app = Flask(__name__, static_folder='static', static_url_path='')
@@ -13,6 +13,26 @@ def index():
 @app.route("/newuser/", methods=['POST'])
 def new_user(name, my_hash):
     return name + '/'   + my_hash
+
+@app.route("/login", methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return render_template('success.html')
+    return render_template('index.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return render_template('index.html')
 
 # initializes DB
 def init_db():
@@ -32,7 +52,15 @@ def teardown_request(exception):
         db.close()
 
 def connect_db():
-    return sqlite4.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/entries')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
