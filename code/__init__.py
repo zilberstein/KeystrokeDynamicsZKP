f1e1a14c2f4e96dbc84564f859211a1531178ab2
jpchen <jonathanp.chen@gmail.com>
2014-11-23 15:28:32 -0500

    initialize db

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+# configuration
+DATABASE = '/tmp/zkp.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'admin'
--- code/server.py
+++ code/server.py
@@ -1,19 +1,38 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, g, redirect, url_for, \
+             abort, render_template, flash
+from contextlib import closing
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+app.config.from_object('__init__')
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/login/<s>")
-def login(s):
-    return s
-
-@app.route("/new/<name>/<my_hash>")
+@app.route("/newuser/", methods=['POST'])
 def new_user(name, my_hash):
-    return name + '/' + my_hash;
+    return name + '/'   + my_hash
 
+# initializes DB
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def connect_db():
+    return sqlite4.connect(app.config['DATABASE'])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
